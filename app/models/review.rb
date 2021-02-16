9d530117f42b9ec88a643b6b98bc58e3f9b15c55
Brandon Lamb <blamb888@gmail.com>
2021-02-16 22:28:21 +0900

    added some restuarants to the db

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   RATING = [1..5]
-  validates :rating, presence:, true inclusion: { in: RATING }
+  validates :rating, presence: true, inclusion: { in: RATING }
   validates :content, presence: true, length: { minimum: 25 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_02_16_124317) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
